7a39a6ca3a8275594460d4c57702c8d63c024379
Atish Shah Rouniyar <atishshahrouniyar@gmail.com>
2021-02-21 22:43:41 +0545

    simple made

--- js/main.js
+++ js/main.js
@@ -3,23 +3,32 @@ const display = document.querySelector('#display');
 
 input.addEventListener('change',displayData);
 
-var index=1;
+var index=0;
 
 function displayData(event){
     let text = document.createElement('tr');
-    text.innerHTML=`<td>${index++}</td> <td>${event.target.value}</td> 
-    <td > <input type="image" src='img/edit.png' height=20 width=20 id='edit${index}' onclick="funcEdit(this)"></td>
-    <td > <input type="image" src='img/delete.png' height=20 width=20 id='delete${index}' onclick="funcDelete(this)"></td>`;
+    text.innerHTML=`<th scope="row">${++index}</th> <td class='data'>${event.target.value}</td> 
+    <td> <input type="image" src='img/edit.png' height=20 width=20 onclick="funcEdit(this)"></td>
+    <td> <input type="image" src='img/delete.png' height=20 width=20 onclick="funcDelete(this)"></td>`;
     display.appendChild(text);
     event.target.value=""; 
 }
 
 function funcDelete(img){
-    document.getElementById(img.id).parentElement.parentElement.remove();
+    img.parentElement.parentElement.remove();
+    console.log(img);
+    updateIndex();
 }
 
 function funcEdit(img) {
-    console.log(img);
-    console.log(img.id);
-    console.log(document.getElementById(img.id).parentElement.parentElement.remove());
+    input.value = img.parentElement.parentElement.children[1].innerText;
+    funcDelete(img);
+    updateIndex();
+}
+
+function updateIndex() {
+    for(var i=0;i<display.children.length;++i){
+        display.children[i].children[0].innerText=i+1;
+    }
+    index=i;
 }
