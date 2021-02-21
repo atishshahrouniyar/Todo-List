8282aaecad18bbba012b56352167069397d9b0ea
Atish Shah Rouniyar <atishshahrouniyar@gmail.com>
2021-02-21 19:30:36 +0545

    delete implemented

--- js/main.js
+++ js/main.js
@@ -7,7 +7,19 @@ var index=1;
 
 function displayData(event){
     let text = document.createElement('tr');
-    text.innerHTML=`<td>${index++}</td> <td>${event.target.value}</td>`;
+    text.innerHTML=`<td>${index++}</td> <td>${event.target.value}</td> 
+    <td > <input type="image" src='img/edit.png' height=20 width=20 id='edit${index}' onclick="funcEdit(this)"></td>
+    <td > <input type="image" src='img/delete.png' height=20 width=20 id='delete${index}' onclick="funcDelete(this)"></td>`;
     display.appendChild(text);
     event.target.value=""; 
+}
+
+function funcDelete(img){
+    document.getElementById(img.id).parentElement.parentElement.remove();
+}
+
+function funcEdit(img) {
+    console.log(img);
+    console.log(img.id);
+    console.log(document.getElementById(img.id).parentElement.parentElement.remove());
 }
